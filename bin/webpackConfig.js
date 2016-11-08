ba29b16fd9d3e25cc5e3414b33d04579f54b4a5f
Jack Lawson <ajacksified@gmail.com>
2016-11-08 11:17:14 -0800

    Initial

---
+++ bin/webpackConfig.js
@@ -0,0 +1,101 @@
+const chalk = require('chalk');
+const webpack = require('webpack');
+const path = require('path');
+
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const externals = require('webpack-node-externals');
+
+module.exports = function (options) {
+  if (typeof options.extensions === 'string') {
+    options.extensions = options.extensions.split(',');
+  }
+
+  const sourceDir = options['source-directory'];
+  const extensions = options.extensions;
+  const destDir = options['dest-directory'];
+  const input = options.input;
+  const output = options.output;
+  const cwd = options.cwd;
+
+  return {
+    target: 'node',
+    externals: [ externals() ],
+    devtool: 'inline-source-map',
+    context: path.join(cwd, sourceDir),
+    entry: {
+      js: path.join(cwd, input),
+    },
+    output: {
+      path: path.join(cwd, destDir),
+      filename: output,
+      libraryTarget: 'commonjs'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              [ 'es2015', { loose: true, modules: false } ],
+              'stage-2',
+            ],
+            plugins: [
+              'syntax-decorators',
+              'transform-class-properties',
+              'transform-decorators-legacy',
+              'transform-decorators',
+            ]
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: extensions,
+      modules: [
+        path.join(cwd, sourceDir),
+      ]
+    },
+    plugins: [
+      new ProgressBarPlugin({
+        clear: false,
+        format: 'build [:bar] ' + chalk.green(':percent') + ' (:elapsed seconds)',
+      }),
+      //new webpack.ProgressPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: true
+      }),
+      new webpack.BannerPlugin({
+        banner: 'require("source-map-support").install();require("regenerator-runtime/runtime");',
+        raw: true,
+        entryOnly: false,
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        debug: true,
+        output: {
+          comments: false
+        },
+        sourceMap: true
+      }),
+    ],
+    node: {
+      global: false,
+      process: false,
+      Buffer: false,
+      crypto: false,
+      module: false,
+      clearImmediate: false,
+      setImmediate: false,
+      clearTimeout: false,
+      setTimeout: false,
+      os: false,
+      _filename: true,
+      __dirname: true,
+    }
+  };
+};
