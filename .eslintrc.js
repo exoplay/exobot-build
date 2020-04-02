c50d3c4d34ace2bc83a5099674ee8a771ee9b620
Jack Lawson <ajacksified@gmail.com>
2020-04-02 11:40:49 -0400

    exobot lint @ 2.2.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,4 @@ module.exports = {
   extends: [
     '@exoplay/eslint-config-exobot',
   ],
-  env: {
-    node: true,
-  },
-  settings: {
-    'import/resolver': {
-      node: {},
-      webpack: {
-        config: path.join(__dirname, 'webpack.config.babel.js'),
-      },
-    },
-  }
 };
